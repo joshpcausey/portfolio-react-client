3a6ce1631cf08dad42ca2ab0dc715345c7fb3f50
= <joshpcausey@gmail.com>
2020-11-10 21:48:30 -0600

    fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders still coding text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/still busy coding this one/i);
   expect(linkElement).toBeInTheDocument();
 });
